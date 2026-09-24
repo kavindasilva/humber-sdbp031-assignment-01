8ab5a117bd314842d5cd5d13b923aa6c6155ca6d
Kavinda Silva <kss.kavinda@gmail.com>
2026-09-23 20:26:45 -0400

    ChatController started

---
+++ AgentAssignment.Server/Controllers/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using AgentAssignment.Server.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgentAssignment.Server.Controllers;
+
+[ApiController]
+[Route("/v1/chat/completions")]
+[Produces("application/json")]
+public class ChatController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType<ChatCompletionResponse>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
+    public async Task<ActionResult<ChatCompletionResponse>> CompleteAsync([FromBody] ChatCompletionRequest request, CancellationToken cancellationToken = default)
+    {
+        return Ok();
+    }
+}
